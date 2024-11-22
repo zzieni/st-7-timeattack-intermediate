4ac2f4ee3e484346c8557ea76bdc89ab8e84ff18
Kim Ji Eun <{>
2024-11-22 16:36:45 +0900

    계산기

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import "./App.css";
+import { useRef } from 'react';
+import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
 
 // TODO:
 // 1. 계산결과를 redux를 이용한 전역상태로 관리해 보세요.
@@ -7,11 +9,29 @@ import "./App.css";
 // useRef 가 어렵다면 useState 를 사용해서 input value를 관리하는 제어 컴포넌트로 하셔도 좋습니다.
 // 단 제어 컴포넌트 사용 시 useRef 를 본인이 아직 사용할 줄 모른다라고 판단할 수 있겠습니다.
 function App() {
+  const inputNum = useRef(null);
+  console.log(inputNum.current);
+
+  const result = useSelector((state) => state.calculator.result);
+  const dispatch = useDispatch();
+
+  const handelAdd = () => {
+    const num = Number(inputNum.current.value);
+    console.log(num, '+');
+    dispatch(add(num));
+  };
+  const handelSub = () => {
+    const num = Number(inputNum.current.value);
+    console.log(num, '-');
+    dispatch(sub(num));
+  };
   return (
     <div className="App">
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
+        <input ref={inputNum} /> 만큼을{' '}
+        <button onClick={handelAdd}>더할게요</button>{' '}
+        <button onClick={handelSub}>뺄게요</button>
       </div>
       <hr />
       <div>
--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,6 +1,9 @@
 // TODO: store 에 slice에서 import한 reducer를 할당하세요.
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
+import onCalculator from '../slices/calculatorSlice';
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    calculator: onCalculator,
+  },
 });
